41dad21f9c7ab8d788500e3d5adf632e47b5297f
4K05H <rbenakos@gmail.com>
2022-03-04 13:20:04 +0100

    bmi

--- src/bmi_kalkuláto/BMI_kalkuláto.java
+++ src/bmi_kalkuláto/BMI_kalkuláto.java
@@ -7,10 +7,14 @@ public class BMI_kalkuláto {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        System.out.println("Adja meg a magasságát: ");
         
+        System.out.println("Adja meg a magasságát: ");
         String magassag = sc.nextLine();
-        System.out.println("asd: " + magassag);
+
+        System.out.println("Adja meg a testsúlyát: ");
+        String suly = sc.nextLine();
+
+        int bmi = suly / (magassag * magassag);
         
     }
     
