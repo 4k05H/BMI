81a4cb58943c38ccb679b27ae268a86d58732aa5
4K05H <rbenakos@gmail.com>
2022-03-04 13:18:50 +0100

    BMI

--- src/bmi_kalkuláto/BMI_kalkuláto.java
+++ src/bmi_kalkuláto/BMI_kalkuláto.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bmi_kalkuláto;
 
-/**
- *
- * @author hallgato
- */
+import java.util.Scanner;
+
 public class BMI_kalkuláto {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adja meg a magasságát: ");
+        
+        String magassag = sc.nextLine();
+        System.out.println("asd: " + magassag);
+        
     }
     
 }
